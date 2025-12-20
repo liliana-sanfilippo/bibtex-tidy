9b9a83925cd006e239248e61c0cb05958eef798d
Liliana Sanfilippo <liliana.sanfilippo@uni-bielefeld.de>
2025-12-20 21:49:46 +0100

    remove numbers from keys try 7

--- bibtex-tidy.js
+++ bibtex-tidy.js
@@ -4974,6 +4974,7 @@ function generateTransformPipeline(options) {
     pipeline.push(createRemoveEmptyFieldsTransform());
     pipeline.push(createPreferCurlyTransform());
     pipeline.push(createRemoveSpecifiedFieldsTransform(["abstract"]));
+    pipeline.push(createGenerateKeysTransform("[auth:required:lower][year:required][veryshorttitle:lower][duplicateNumber]"));
     pipeline.push(createDropAllCapsTransform());
   }
   if (options.dropAllCaps) {
